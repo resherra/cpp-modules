daf469bfa8cdf1f74674e707b9464846f3c810fe
resherra <redouancherrat@gmail.com>
2024-12-28 20:43:54 +0100

    <<

--- CPP-01/ex04/main.cpp
+++ CPP-01/ex04/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 void    replace(std::string buffer, std::ofstream& outfile, std::string to_replace, std::string replace_by)
 {   
-    static int i = 0;
     int to_replace_len = to_replace.length();
     std::string newString = buffer;
     size_t position = newString.find(to_replace, 0);
@@ -14,11 +14,7 @@ void    replace(std::string buffer, std::ofstream& outfile, std::string to_repla
         newString = newString.insert(position, replace_by); 
         position = newString.find(to_replace, position + replace_by.length() - 1);
     }
-    if (i == 0)
-        outfile << newString;
-    else
-        outfile << std::endl << newString;
-    i++;
+    outfile << newString;
 }
 
 int main(int ac, char **av)
@@ -32,18 +28,16 @@ int main(int ac, char **av)
     std::string s1 = av[2];
     std::string s2 = av[3];
  
-    std::ifstream foo(filename);   
-    if (!foo)
+    std::ifstream file(filename);   
+    if (!file)
     {
         std::cerr << "Can't open the file!" << std::endl;
         return 1;
     }
-
-
-    std::ifstream file(filename);   
     std::string suffix = ".replace";
     std::ofstream outfile(filename + suffix);
-    std::string buffer;
-    while (std::getline(file, buffer))
-        replace(buffer, outfile, s1, s2);
+    std::stringstream buffer;
+
+    buffer << file.rdbuf();
+    replace(buffer.str(), outfile, s1, s2);
 }
