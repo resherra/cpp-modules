74a28cbfd518ece514bca9fb8560a344bea88173
resherra <redouancherrat@gmail.com>
2025-01-16 11:58:25 +0100

    <<

--- CPP-02/ex03/main.cpp
+++ CPP-02/ex03/main.cpp
@@ -10,14 +10,16 @@ void    print(std::string str)
 
 int main( void )
 {
-    Point a(1, 2);
-    Point b(4, 6);
-    Point c(6, 2);
+    Point a(1.25, 1.75);
+    Point b(4.25, 5.75);
+    Point c(6.25, 1.75);
 
-    Point   point1(3.7, 3.6);
-    Point   point2(10.8, 4.4);
+    Point   point1(3.25, 3.75); //inside
+    Point   point2(2.25, 2.75); //inside
+    Point   point3(5.25, 4.75); //outside
 
     std::cout << (bsp(a, b, c, point1) ? "the point is inside the triangle" : "the point is outside the triangle") << '\n';
     std::cout << (bsp(a, b, c, point2) ? "the point is inside the triangle" : "the point is outside the triangle") << '\n';
+    std::cout << (bsp(a, b, c, point3) ? "the point is inside the triangle" : "the point is outside the triangle") << '\n';
     return 0;
 }
